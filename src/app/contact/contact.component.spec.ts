4eecf93bb1cc7c015ea9f252b50e9aca5effa8b6
ilya0 <iosovets@gmail.com>
2019-01-16 12:54:32 -0800

    template finished and working

--- src/app/contact/contact.component.spec.ts
+++ src/app/contact/contact.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactComponent } from './contact.component';
 
+
+
 describe('ContactComponent', () => {
   let component: ContactComponent;
   let fixture: ComponentFixture<ContactComponent>;
