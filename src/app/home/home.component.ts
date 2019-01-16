ee022b0cc33521d1d2daa0b7b9d34ce7abaaf7a4
ilya0 <iosovets@gmail.com>
2019-01-16 10:38:06 -0800

    added styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  h1Style: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  firstClick(){
+    console.log('clicked');
+    this.h1Style = true;
   }
 
 }
